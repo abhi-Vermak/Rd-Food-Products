a266423c840fe6ad9c38f0f2a403b4191e6f7b6b
Abhishek Verma <abhishekverma6533@gmail.com>
2024-01-11 21:08:23 +0530

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
-import Raf from './component/Raf';
-import Home from './component/home/Home';
-import Navbar from "./component/navbar/Navbar";
+import Navbar from './component/navbar/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar/>
-      {/* <Home/> */}
     </div>
   );
 }
 
 export default App;
-{/* <Raf/> */}
+
--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -1,45 +1,68 @@
-
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import "./navbar.css";
 
-export default function Navbar() {
-  const [isMenuVisible, setMenuVisibility] = useState(false);
+export default function () {
+    const [menu ,setMenu] = useState(null);
+    const[menuButton, setMenuButton] = useState(null);
+
+    useEffect(()=>{
+        const navMenu = document.querySelector(".navbarItems");
+
+        const menubtn = document.querySelector(".menu");
+        setMenu(navMenu);
+        setMenuButton(menubtn);
+        
+    },[])
 
   const showMenu = () => {
-    // setMenuVisibility(!isMenuVisible);
-    console.log("button clicked")
+    console.log("click me ");
+    menu.classList.toggle("visible");
+    menuButton.classList.toggle("show");
+ 
   };
-   
+
   return (
-    <div className="navbar">
-      <div className="logo">
-        <img src="/Images/Logo.png" alt="logo"/>
+    <>
+      {/* Navbar */}
+      <div className="navbar">
+        {/* Navbar Logo image */}
+        <div className="logo">
+          <img src="./Images/Logo.png" alt="Logo" className="logoImg" />
+        </div>
+
+        <div className="navbarItems">
+          <ul className="itemList">
+            <li className="item">Namkeen</li>
+            <li className="item">Navratra Items</li>
+            <li className="item">Maida Items</li>
+            <li className="item">Fried</li>
+            <li className="item">Sweets</li>
+            <li className="item">Gifting</li>
+          </ul>
+          {/* OtherItems */}
+          <ul className="otherItems">
+
+            <li>
+              <i className="fa-solid fa-search"></i>
+            </li>
+            <li>
+              <i className="fa-solid fa-cart-shopping"></i>
+            </li>
+
+            <li className="login">Login</li>
+          </ul>
+
+          {/* close Button */}
+          <button onClick={showMenu} className="cutButton">
+            <i className="fa-solid fa-x"></i>
+          </button>
+        </div>
       </div>
 
-      <ul className={`itemList ${isMenuVisible ? "visible" : ""}`}>
-
-        <li className="items">Namkeen</li>
-        <li className="items">Maida Items</li>
-        <li className="items">Snacks</li>
-        <li className="items">Sweets</li>
-        <li className="items">Fried Items</li>
-        <li className="items">Navratra Items</li>
-        <li className="items">Dry Fruits</li>
-        <li className="items">Giftings</li>
-        <li className="items">About Us</li>
-  
-
-      <div className="rightSide">
-        <li className="searchBox items">
-            <input type="text" id="search" name="search" placeholder="Search Items"/>
-          <i className="fa-solid fa-search"></i>
-        </li>
-        <li className="items"> 
-          <i className="fa-solid fa-cart-shopping"></i>
-        </li>
-        <li className="items">Login</li>
-      </div>
-      </ul>
-      </div>
+      {/* Menu button */}
+      <button onClick={showMenu} className="menu">
+        <i className="fa-solid fa-bars"></i>
+      </button>
+    </>
   );
 }
