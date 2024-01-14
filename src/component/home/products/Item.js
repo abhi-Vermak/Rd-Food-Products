eb2289b449c88cbc31eee9517e76066bbcd1834e
Abhishek Verma <abhishekverma6533@gmail.com>
2024-01-14 19:41:56 +0530

    navbar and carasoule completed

--- src/component/home/products/Item.js
+++ src/component/home/products/Item.js
@@ -4,6 +4,7 @@ import "./item.css"
 export default function Item() {
   return (
     <div className='itemContainer'>
+      <h2>Namkeen</h2>
         <img src="/Images/Products/Namkeen/Kaju Mix.jpg" alt="Namkeen"/>
         <span >Kaju Mixture</span>
     </div>
